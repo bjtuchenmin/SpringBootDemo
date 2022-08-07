903065001b8ce43602be36279ba2b83d244c5e6f
bjtuchenmin <1315559425@qq.com>
2022-08-07 11:34:40 +0800

    打成 jar 包，使用 jib 插件编译 docker image，并推送到 dockerhub

--- demo/customer/src/main/java/com/example/customer/CustomerApplication.java
+++ demo/customer/src/main/java/com/example/customer/CustomerApplication.java
@@ -12,7 +12,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
         }
 )
 @EnableEurekaClient
-@EnableFeignClients(basePackages = "com.example.clients.fraud")
+@EnableFeignClients(basePackages = "com.example.clients")
 public class CustomerApplication {
     public static void main(String[] args) {
         SpringApplication.run(CustomerApplication.class, args);
